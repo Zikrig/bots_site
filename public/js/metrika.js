c25a6c7b634ff8a92f2be77abefeeeef0a77d382
Fedor Grizik <fedorgrif@gmail.com>
2026-02-24 01:26:09 +0300

    metricz

---
+++ public/js/metrika.js
@@ -0,0 +1,27 @@
+// Yandex Metrika loader for lead submissions.
+function runYandexMetrikaCounter() {
+    window.dataLayer = window.dataLayer || [];
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return; }
+        }
+        k=e.createElement(t);
+        a=e.getElementsByTagName(t)[0];
+        k.async=1;
+        k.src=r;
+        a.parentNode.insertBefore(k,a);
+    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=106972892', 'ym');
+
+    ym(106972892, 'init', {
+        ssr: true,
+        webvisor: true,
+        clickmap: true,
+        ecommerce: "dataLayer",
+        referrer: document.referrer,
+        url: location.href,
+        accurateTrackBounce: true,
+        trackLinks: true
+    });
+}
